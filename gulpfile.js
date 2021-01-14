066191df64499e253fb79c9a8fbce0fd0adeab51
cunning-seal <a.titov9996@gmail.com>
2021-01-14 13:33:19 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const rename = require("gulp-rename");
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    })
+})
+
+gulp.task('styles', function(){
+    return gulp.src("src/sass/*.+(sass|scss)")
+        .pipe(sass())
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+          }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+})
+
+
+gulp.task('watch', function () {
+    gulp.watch("src/sass/*.+(sass|scss)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('server', 'styles', 'watch'));
+
